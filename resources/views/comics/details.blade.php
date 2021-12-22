6dafefe78b9557e6771a10df74c391f73e5b02ea
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-12-22 09:16:42 +0100

    details

--- resources/views/comics/details.blade.php
+++ resources/views/comics/details.blade.php
@@ -1,17 +1,17 @@
-@extends('layouts.layout_default')
+@extends('layouts.layout_details')
 
 @section('page-title', 'DC Comics - ' . $comic["title"])
 
 @section('content')
-    <div class="container">
-        <div class="row" style="color: white">
+    <div class="container bg-white">
+        <div class="row py-5">
             <div class="col-8">
                 <h1 >{{ $comic["title"]}}</h1>
-                <div class="price"> U.S. Price {{ $comic["price"]}}</div>
+                <div class="price my-4 bg-success"> U.S. Price {{ $comic["price"]}}</div>
                 <p>{{ $comic["description"]}}</p>
             </div>
             <div class="col-4">
-                <h3>ADVERTISEMENT</h3>
+                <h6 class="text-center">ADVERTISEMENT</h6>
                 <img src="{{asset('img/picture.jpg')}}" alt="poster">
             </div>
         </div>
---
+++ resources/views/layouts/layout_details.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('page-title')</title>
+    @include("partials.header_scripts")
+
+</head>
+<body>
+    @include("partials.the_header")
+
+    <main class="content-container bg-white">
+        <div class="jumbotron">
+            <img src="{{asset("img/jumbotron.jpg")}}" alt="jumbo-img" />
+          </div>
+        @yield('content')
+    </main>
+
+    @include("partials.footer_details")
+</body>
+</html>
