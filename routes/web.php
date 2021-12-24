878eecf7c5fb75c19cebdfff8388f92633c0dc6e
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-12-24 15:38:02 +0100

    home controller

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view("home.index");
-})->name("home.index");
+Route::get('/', 'HomeController@index')->name("home.index");
 
 Route::get('/characters', function () {
     return view("characters.index");
